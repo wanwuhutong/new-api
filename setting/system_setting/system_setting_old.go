95eab6360248b33c6136134c90dca8e2ddaf048d
user2758629121 <674414686375284-user2758629121@noreply.coze.cn>
2026-06-11 15:02:32 +0800

    feat: remove footer GitHub link and update default server address
    Coze-Commit-Type: user
    Coze-User-ID: 674414686375284
    Coze-Conversation-ID: 6697089

--- setting/system_setting/system_setting_old.go
+++ setting/system_setting/system_setting_old.go
@@ -1,6 +1,6 @@
 package system_setting
 
-var ServerAddress = "http://localhost:3000"
+var ServerAddress = "https://www.intokenclear.com"
 var WorkerUrl = ""
 var WorkerValidKey = ""
 var WorkerAllowHttpImageRequestEnabled = false
